f5a3f1c21426245076b9bafb28fade78320cfa22
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-26 16:55:23 +0300

    Create store category repository and service.

---
+++ src/main/java/com/acme/team7/ACMEDeliveryService/service/StoreCategoryService.java
@@ -0,0 +1,6 @@
+package com.acme.team7.ACMEDeliveryService.service;
+
+import com.acme.team7.ACMEDeliveryService.domain.StoreCategory;
+
+public interface StoreCategoryService extends BaseService<StoreCategory>{
+}
