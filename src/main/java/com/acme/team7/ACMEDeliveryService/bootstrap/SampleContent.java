5f87e077a64b23d15b5eec4c9ba24122f377287b
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-26 17:21:57 +0300

    add log4j2.xml and sample content for store and store category.

--- src/main/java/com/acme/team7/ACMEDeliveryService/bootstrap/SampleContent.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/bootstrap/SampleContent.java
@@ -1,16 +1,14 @@
 package com.acme.team7.ACMEDeliveryService.bootstrap;
 
 import com.acme.team7.ACMEDeliveryService.base.BaseComponent;
-import com.acme.team7.ACMEDeliveryService.domain.Account;
-import com.acme.team7.ACMEDeliveryService.domain.Order;
-import com.acme.team7.ACMEDeliveryService.domain.PaymentMethod;
-import com.acme.team7.ACMEDeliveryService.service.AccountService;
-import com.acme.team7.ACMEDeliveryService.service.OrderService;
-import com.acme.team7.ACMEDeliveryService.service.ProductService;
+import com.acme.team7.ACMEDeliveryService.domain.*;
+import com.acme.team7.ACMEDeliveryService.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -19,9 +17,35 @@ public class SampleContent extends BaseComponent implements CommandLineRunner {
     private final AccountService accountService;
     private final OrderService orderService;
     private final ProductService productService;
+    private final StoreService storeService;
+    private final StoreCategoryService storeCategoryService;
 
     @Override
     public void run(String... args) throws Exception {
+
+        //Implement below Accounts
+
+
+        StoreCategory tavern = storeCategoryService.create(StoreCategory.builder().description("Tavern").build());
+        StoreCategory coffeeShop = storeCategoryService.create(StoreCategory.builder().description("Coffee Shop").build());
+        StoreCategory iceCreamShop = storeCategoryService.create(StoreCategory.builder().description("Ice Cream Shop").build());
+        StoreCategory souvlatzidiko = storeCategoryService.create(StoreCategory.builder().description("Souvlatzidiko").build());
+        storeCategoryService.findAll().forEach(sc -> log.info("Store category {} created.", sc));
+
+        //functionaliti na min mporei na paragilei apo makria??
+        List<Store> storeList = List.of(
+                Store.builder().name("Louloudaki").address("Plateia Eleftheris 51, 71303 Athens Attiki").storeCategory(tavern).build(),
+                Store.builder().name("Xelona").address("Analipsi 7, 70303 Athens Attiki").storeCategory(tavern).build(),
+                Store.builder().name("Kagiampis").address("Plateia Kornarou 51, 71203 Athens Attiki").storeCategory(souvlatzidiko).build(),
+                Store.builder().name("Louloudaki").address("Dikaiosinis 51, 70201 Athens Attiki").storeCategory(iceCreamShop).build());
+        log.info("Created stores. {}",storeService.createAll(storeList));
+
+        //Implement below product category
+
+        //Implement below products
+
+        //Set products to stores
+
         accountService.findAll().forEach(c -> log.info("{}", c));
 
         log.info("Does customer exist? {}.", (accountService.findByEmail("c.giannacoulis@codehub.gr") != null));
@@ -30,9 +54,9 @@ public class SampleContent extends BaseComponent implements CommandLineRunner {
         // Load customer and create an order by adding/updating/removing content before checking it out
         Account firstAccount = accountService.findByEmail("c.giannacoulis@codehub.gr");
         Order firstOrder = orderService.initiateOrder(firstAccount);
-
+//find by name and by store??
         orderService.addItem(firstOrder, productService.findByName("Louloudaki Salad"), 2);
-        orderService.addItem(firstOrder, productService.findByName("Livers"), 1);
+        orderService.addItem(firstOrder, productService.findByName("Livers"), 1); //findByNameAndStore_Id ?????
         orderService.addItem(firstOrder, productService.findByName("Fried Zucchini"), 1);
 //        // Update item(s)
 //        orderService.addItem(firstOrder, productService.findByName("Livers"), 1);
