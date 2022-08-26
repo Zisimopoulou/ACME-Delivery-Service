f247895cb0620bd40b5b9c337d86bbf9ff9445cb
Petros Dhespollari <peterdsp@icloud.com>
2022-08-26 12:51:00 +0300

    Update AccountService

--- src/main/java/com/acme/team7/ACMEDeliveryService/service/AccountService.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/service/AccountService.java
@@ -1,8 +1,6 @@
 package com.acme.team7.ACMEDeliveryService.service;
 
 import com.acme.team7.ACMEDeliveryService.domain.Account;
-import com.acme.team7.ACMEDeliveryService.repository.AccountRepository;
 
 public interface AccountService extends BaseService<Account> {
-    public AccountRepository getRepository();
 }
