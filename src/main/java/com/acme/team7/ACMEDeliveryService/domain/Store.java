d03529f6cb7a74a74cdfcd51f0cab7cb07b739e9
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-26 13:53:03 +0300

    change sequence name in store class.

--- src/main/java/com/acme/team7/ACMEDeliveryService/domain/Store.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/domain/Store.java
@@ -3,6 +3,7 @@ package com.acme.team7.ACMEDeliveryService.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,7 +13,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "STORES")
-@SequenceGenerator(name = "idGenerator", sequenceName = "STORES", initialValue = 1, allocationSize = 1)
+@SequenceGenerator(name = "idGenerator", sequenceName = "STORES_SEQ", initialValue = 1, allocationSize = 1)
 public class Store extends BaseModel {
 
     @ToString.Exclude
@@ -23,9 +24,6 @@ public class Store extends BaseModel {
 
     private String address;
 
-
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     private StoreCategory storeCategory;
-
-    private String review;
 }
