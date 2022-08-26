fcb1fba32389148794f906eed034d88265bfd796
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-26 15:47:08 +0300

    Delete not used import of packages.

--- src/main/java/com/acme/team7/ACMEDeliveryService/repository/ProductRepository.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/repository/ProductRepository.java
@@ -1,9 +1,7 @@
 package com.acme.team7.ACMEDeliveryService.repository;
 
-import com.acme.team7.ACMEDeliveryService.domain.Account;
 import com.acme.team7.ACMEDeliveryService.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.QueryHints;
 
 import java.util.List;
 
