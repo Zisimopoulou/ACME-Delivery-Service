e8ddaf30e8fab5647d9435c0aa159bf2760292a7
Petros Dhespollari <peterdsp@icloud.com>
2022-08-29 11:10:07 +0300

    Update AccountRepository with findbyEmail & findbyPhoneNumber

--- src/main/java/com/acme/team7/ACMEDeliveryService/repository/AccountRepository.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/repository/AccountRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    Account findByEmail(String email);
+    Account findByPhoneNumber(String phoneNumber);
 }
