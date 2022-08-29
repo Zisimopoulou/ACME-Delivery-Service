fb0a33a3f2daba36b395ed53f317074211854648
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-29 22:02:55 +0300

    Add json ignore to store attribute in Product.

--- src/main/java/com/acme/team7/ACMEDeliveryService/domain/Product.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/domain/Product.java
@@ -1,11 +1,13 @@
 package com.acme.team7.ACMEDeliveryService.domain;
 
 import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+
 @Getter
 @Setter
 @ToString(callSuper = true)
@@ -16,20 +18,26 @@ import java.math.BigDecimal;
 @Table(name = "PRODUCTS", indexes = {@Index(name = "PRODUCT_IDX_01", columnList = "serial"),})
 @SequenceGenerator(name = "idGenerator", sequenceName = "PRODUCTS_SEQ", initialValue = 1, allocationSize = 1)
 public class Product extends BaseModel {
+
     @Column(length = 30, nullable = false, unique = true)
     @NotEmpty
     private String serial;
+
     @Column(length = 50, nullable = false)
     @NotEmpty
     private String name;
+
     @Column(precision = 10, scale = 2,nullable = false)
     @NotNull
     private BigDecimal price;
+
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @NotNull
     private ProductCategory productcategory;
+
     @ManyToOne(fetch = FetchType.EAGER,optional=false)
     @NotNull
+    @JsonIgnore
     private Store store;
 
 }
