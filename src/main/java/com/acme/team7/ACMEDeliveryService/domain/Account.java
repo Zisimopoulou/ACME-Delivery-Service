0160865d1c5236aedee11cb2e01e7b687cfb60d8
Petros Dhespollari <peterdsp@icloud.com>
2022-08-29 17:09:49 +0300

    Update Account

--- src/main/java/com/acme/team7/ACMEDeliveryService/domain/Account.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/domain/Account.java
@@ -1,14 +1,9 @@
 package com.acme.team7.ACMEDeliveryService.domain;
 
 import lombok.*;
-import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
@@ -22,14 +17,17 @@ import javax.validation.constraints.NotNull;
 public class Account extends BaseModel{
     @Column(length = 30, nullable = false)
     @NotNull(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name must be alphabetic")
     private String firstName;
 
     @NotNull(message = "Last name is required")
     @Column(length = 30, nullable = false)
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Last name must be alphabetic")
     private String lastName;
 
     @NotNull(message = "Address is required")
     @Column(length = 30, nullable = false)
+    @Pattern(regexp = "^[a-zA-Z0-9 ]*$$", message = "Address must be alphabetic")
     private String address;
 
     @NotNull(message = "Email number is required")
@@ -39,11 +37,14 @@ public class Account extends BaseModel{
 
     @NotNull(message = "Password is required")
     @Column(length = 30, nullable = false)
+    @Pattern(regexp = "\"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()Ã¢Â€Â“[{}]:;',?/*~$^+=<>]).{8,20}$\", \n" +
+            "         message = \"Invalid Password pattern" +
+            ". Password must contain 8 to 20 characters at least one digit" +
+            ", lower, upper case and one special character.\"")
     private String password;
 
     @NotNull(message = "Phone number is required")
     @Column(length = 10, nullable = false, unique = true)
-    @NumberFormat(pattern = "69########")
     private String phoneNumber;
 
     @Max(value = 140, message = "too old for this application")
