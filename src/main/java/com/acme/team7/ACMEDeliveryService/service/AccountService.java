821efaf476f70989adcf3acb02f203687efaf9e4
Petros Dhespollari <peterdsp@icloud.com>
2022-08-29 11:10:32 +0300

    Update AccountService with findbyEmail & findbyPhoneNumber

--- src/main/java/com/acme/team7/ACMEDeliveryService/service/AccountService.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/service/AccountService.java
@@ -3,4 +3,6 @@ package com.acme.team7.ACMEDeliveryService.service;
 import com.acme.team7.ACMEDeliveryService.domain.Account;
 
 public interface AccountService extends BaseService<Account> {
+    Account findByEmail(String email);
+    Account findByPhoneNumber(String phoneNumber);
 }
