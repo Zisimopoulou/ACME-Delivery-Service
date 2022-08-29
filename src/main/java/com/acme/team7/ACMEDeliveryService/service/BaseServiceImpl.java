4603f2ba15a8e8a4d574b3c36a45bd214feedd73
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-29 21:34:01 +0300

    Add transactional annotation to BaseServiceImpl and get referenced by id in delete methods. Delete static from create method.

--- src/main/java/com/acme/team7/ACMEDeliveryService/service/BaseServiceImpl.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/service/BaseServiceImpl.java
@@ -4,6 +4,8 @@ import com.acme.team7.ACMEDeliveryService.base.BaseComponent;
 import com.acme.team7.ACMEDeliveryService.domain.BaseModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
@@ -11,6 +13,8 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Slf4j
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, rollbackFor =
+        Exception.class)
 public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent implements BaseService<T> {
    public abstract JpaRepository<T, Long> getRepository();
 
@@ -39,13 +43,16 @@ public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent
 
    @Override
    public void delete(final T item){
-      log.trace("Deleting item {}.",item);
-      getRepository().delete(item);
+      final T itemFound = getRepository().getReferenceById(item.getId());
+      log.trace("Deleting {}.", itemFound);
+      getRepository().delete(itemFound);//why not delete(item)
+
    }
 
    @Override
    public void deleteById(final Long id){
-      log.trace("Deleting item {}",get(id));
+      final T itemFound = getRepository().getReferenceById(id);
+      log.trace("Deleting {}.", itemFound);
       getRepository().deleteById(id);
    }
 
@@ -53,7 +60,7 @@ public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent
    @Override
    public T get(final Long id){
       log.trace("Obtaining item with id {}.",id);
-      return getRepository().findById(id).orElseThrow(() -> new NoSuchElementException("Item not found"));
+      return getRepository().findById(id).orElseThrow(() -> new NoSuchElementException("Item not found."));
    }
 
    @Transactional(readOnly = true)
