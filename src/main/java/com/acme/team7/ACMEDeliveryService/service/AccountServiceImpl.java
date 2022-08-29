abafbe1e13bcd560f4f297f909048f789b90cdb7
Petros Dhespollari <peterdsp@icloud.com>
2022-08-29 11:10:59 +0300

    Update AccountServiceImpl with findbyEmail & findbyPhoneNumber

--- src/main/java/com/acme/team7/ACMEDeliveryService/service/AccountServiceImpl.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/service/AccountServiceImpl.java
@@ -1,7 +1,6 @@
 package com.acme.team7.ACMEDeliveryService.service;
 
 import com.acme.team7.ACMEDeliveryService.domain.Account;
-import com.acme.team7.ACMEDeliveryService.domain.Order;
 import com.acme.team7.ACMEDeliveryService.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,5 +15,14 @@ public class AccountServiceImpl extends BaseServiceImpl<Account> implements Acco
     @Override
     public JpaRepository<Account, Long> getRepository() {
         return accountRepository;
+    }
+
+    @Override
+    public Account findByEmail(final String email) {
+        return accountRepository.findByEmail(email);
+    }
+    @Override
+    public Account findByPhoneNumber(final String phoneNumber) {
+        return accountRepository.findByPhoneNumber(phoneNumber);
     }
 }
