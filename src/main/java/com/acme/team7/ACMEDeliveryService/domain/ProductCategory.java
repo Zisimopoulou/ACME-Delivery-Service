5e7a7352c89337b0dd1f83ead5d0c1c1a5407d39
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-30 11:27:43 +0300

    Fix bug by changing the table name of product categories.

--- src/main/java/com/acme/team7/ACMEDeliveryService/domain/ProductCategory.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/domain/ProductCategory.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "PRODUCT_CATEGORIES")
+@Table(name = "PRODUCT_CATEGORIESS")
 @SequenceGenerator(name = "idGenerator", sequenceName = "PRODUCT_CATEGORIES_SEQ", initialValue = 1, allocationSize = 1)
 public class ProductCategory extends BaseModel{
     @Column(length=50,nullable = false)
