9ac81e980020312295aa431d25cd004e4dac011b
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-08-31 17:16:43 +0300

    Create and implement store controller.

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreController.java
@@ -1,4 +1,14 @@
 package com.acme.team7.ACMEDeliveryService.controller;
 
-public class StoreController {
+import com.acme.team7.ACMEDeliveryService.domain.Store;
+import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public interface StoreController {
+    ResponseEntity<ApiResponse<List<Store>>> findByName(@RequestParam String name);
+
+    ResponseEntity<ApiResponse<List<Store>>> findStoreByStoreCategory_Description(@RequestParam String description);
 }
---
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreControllerImpl.java
@@ -0,0 +1,49 @@
+package com.acme.team7.ACMEDeliveryService.controller;
+
+import com.acme.team7.ACMEDeliveryService.domain.Store;
+import com.acme.team7.ACMEDeliveryService.service.BaseService;
+import com.acme.team7.ACMEDeliveryService.service.StoreService;
+import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("stores")
+public class StoreControllerImpl extends BaseControllerImpl<Store> implements StoreController {
+
+    private final StoreService storeService;
+
+    @Override
+    public BaseService<Store> getBaseService() {
+        return storeService;
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<List<Store>>> findByName(@RequestParam String name) {
+        final List<Store> stores = storeService.findByName(name);
+        //prepei na exoume xexoristo exception gia kathe classi?
+        if (name == null) {
+            throw new NoSuchElementException("Unable to find a store with null name.");
+        }
+        return ResponseEntity.ok(ApiResponse.<List<Store>>builder().data(stores).build());
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<List<Store>>> findStoreByStoreCategory_Description(String description) {
+        final List<Store> stores = storeService.findByName(description);
+        //prepei na exoume xexoristo exception gia kathe classi?
+        if (description == null) {
+            throw new NoSuchElementException("Unable to find a store with null description.");
+        }
+        return ResponseEntity.ok(ApiResponse.<List<Store>>builder().data(stores).build());
+    }
+
+
+}
