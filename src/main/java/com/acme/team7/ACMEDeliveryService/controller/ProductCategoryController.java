08d4b4c7a15cbc8a93396bfbe01316330ab24325
d.paliogiannis <oldjohn0000@gmail.com>
2022-08-31 11:03:05 +0300

    CONTROLLER PRODUCTS

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/ProductCategoryController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/ProductCategoryController.java
@@ -1,4 +1,27 @@
 package com.acme.team7.ACMEDeliveryService.controller;
+import com.acme.team7.ACMEDeliveryService.domain.Product;
+import com.acme.team7.ACMEDeliveryService.service.ProductService;
+import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.NoSuchElementException;
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("Products")
 
 public class ProductCategoryController {
-}
+    private final ProductService productService;
+    @GetMapping(params = "Name")
+    public ResponseEntity<ApiResponse<Product>> findProductByName(@RequestParam String name) {
+        final Product Byname = productService.findByName(name);
+        if (Byname== null) {
+            throw new NoSuchElementException("Name not found");
+        }
+        return  ResponseEntity.ok(ApiResponse.<Product>builder().data(Byname).build());
+    }
+
+    }
