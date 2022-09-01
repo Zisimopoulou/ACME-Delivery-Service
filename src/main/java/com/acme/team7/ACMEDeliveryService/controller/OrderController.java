7fb9f617f3c44b0c95f8c5258f1ce488e01a74f3
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-01 16:45:18 +0300

    Create order controller and order controller implementation.

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/OrderController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/OrderController.java
@@ -1,4 +1,13 @@
 package com.acme.team7.ACMEDeliveryService.controller;
 
-public class OrderController {
+import com.acme.team7.ACMEDeliveryService.domain.Order;
+import com.acme.team7.ACMEDeliveryService.domain.PaymentMethod;
+import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+public interface OrderController extends BaseController<Order> {
+
 }
---
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/OrderControllerImpl.java
@@ -0,0 +1,37 @@
+package com.acme.team7.ACMEDeliveryService.controller;
+
+import com.acme.team7.ACMEDeliveryService.domain.Order;
+import com.acme.team7.ACMEDeliveryService.service.BaseService;
+import com.acme.team7.ACMEDeliveryService.service.OrderService;
+import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("orders")
+public class OrderControllerImpl extends BaseControllerImpl<Order> implements OrderController{
+
+    private final OrderService orderService;
+
+    @Override
+    public BaseService<Order> getBaseService() {
+        return orderService;
+    }
+
+    //Afto linei to provlima??
+    @Override
+    @PostMapping
+    public ResponseEntity<ApiResponse<Order>> create(@Valid @RequestBody final Order entity) {
+        return new ResponseEntity<>(ApiResponse.<Order>builder().data(orderService.checkout(entity)).build(),
+                HttpStatus.CREATED);
+    }
+}
+
