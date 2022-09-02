cb1fff5113cc40f77a36ab64623838f879ce99f2
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-02 17:20:41 +0300

    Add messages and constrains to payment card attribute annotations.

--- src/main/java/com/acme/team7/ACMEDeliveryService/domain/PaymentCard.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/domain/PaymentCard.java
@@ -2,12 +2,14 @@ package com.acme.team7.ACMEDeliveryService.domain;
 
 import lombok.*;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.text.SimpleDateFormat;
 
 @Component
@@ -18,19 +20,21 @@ import java.text.SimpleDateFormat;
 @AllArgsConstructor
 public class PaymentCard {
     @CreditCardNumber
-    @NotNull
-    @Pattern(regexp="(^$|[0-9]{10})")
+    @NotNull(message = "Card number is required.")
+    @Size(min=16, max=16,message = "Sixteen digits are required.")
+    @Pattern(regexp="(^$|[0-9]+)", message = "Only integer numbers are acceptable.")
     private String cardNumber;
 
-    @NotNull
-    @Pattern(regexp="(^$|[0-9]{3})")
+    @NotNull(message = "CVC code is required.")
+    @Size(min=3, max=3,message = "Three digits are required.")
+    @Pattern(regexp="(^$|[0-9]+)",message = "Only integer numbers are acceptable.")
     private String cvcCode;
 
-    @NotNull
+    @NotNull(message = "Expiration date is required.")
     @Past
     private SimpleDateFormat expirationDate;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = 50, nullable = false)
     @NotNull(message = "Full name is required.")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must be alphabetic.")
     private String name;
