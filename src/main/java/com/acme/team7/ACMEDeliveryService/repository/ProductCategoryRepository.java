65fc0974fb53c9a37288d04e9880719251a2c58a
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-02 21:04:59 +0300

    Delete product category repo.

--- src/main/java/com/acme/team7/ACMEDeliveryService/repository/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.team7.ACMEDeliveryService.repository;
-
-import com.acme.team7.ACMEDeliveryService.domain.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-}
