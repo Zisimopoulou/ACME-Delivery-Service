a65c2a56e51ac44f0636a3845a9477ab989665a5
Petros Dhespollari <peterdsp@icloud.com>
2022-09-05 14:04:34 +0300

    Update ProductControllerImpl

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/ProductController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.acme.team7.ACMEDeliveryService.domain.Product;
 import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
+
 public interface ProductController extends BaseController<Product> {
     ResponseEntity<ApiResponse<Product>> findProductByName(@RequestParam String name);
 }
