2cc96d472e0deea56171076f82ca2d28c268dd40
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-05 10:59:54 +0300

    Update sample content

--- src/main/java/com/acme/team7/ACMEDeliveryService/bootstrap/SampleContent.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/bootstrap/SampleContent.java
@@ -22,7 +22,6 @@ public class SampleContent extends BaseComponent implements CommandLineRunner {
     private final ProductService productService;
     private final StoreService storeService;
     private final StoreCategoryService storeCategoryService;
-    private final ProductCategoryService productCategoryService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -40,12 +39,26 @@ public class SampleContent extends BaseComponent implements CommandLineRunner {
                 Account.builder().firstName("Nikolas").lastName("Tassides")
                         .address("Rovertou Galli 4 11742 Athina Attica").email("lev12@typestring.com")
                         .password("44&4C&b7Z23").phoneNumber("6948361237").age(19).build());
-                log.info("Accounts created: {}.", accountService.createAll(accountList));
+        log.info("Accounts created: {}.", accountService.createAll(accountList));
 
         accountService.findAll().forEach(c -> log.info("{}", c));
 
         log.info("Does customer email exist? {}.", (accountService.findByEmail("neeneemusic@schule-breklum.de") != null));
         log.info("Does customer email exist? {}.", (accountService.findByEmail("mommykmr@hotmail.red") != null));
+
+        Product xoriatiki = productService.create(Product.builder().name("xoriatiki").productcategory(ProductCategory.SALAD).build());
+        Product liver = productService.create(Product.builder().name("liver").productcategory(ProductCategory.APPETISERS).build());
+        Product zucchini = productService.create(Product.builder().name("zucchini").productcategory(ProductCategory.APPETISERS).build());
+        Product redWine = productService.create(Product.builder().name("redWine").productcategory(ProductCategory.DRINKS).build());
+        Product souvlakiKotopoulo = productService.create(Product.builder().name("souvlakiKotopoulo").productcategory(ProductCategory.SOUVLAKI).build());
+        Product vanilliaFlavor = productService.create(Product.builder().name("vanilliaFlavor").productcategory(ProductCategory.ICECREAM).build());
+        Product chocolateFlavor = productService.create(Product.builder().name("chocolateFlavor").productcategory(ProductCategory.ICECREAM).build());
+        Product greekCoffee = productService.create(Product.builder().name("greekCoffee").productcategory(ProductCategory.DRINKS).build());
+        Product papoutsakia = productService.create(Product.builder().name("papoutsakia").productcategory(ProductCategory.OVEN).build());
+        Product paidakia = productService.create(Product.builder().name("tzatziki").productcategory(ProductCategory.ROASTMEAT).build());
+        Product americano = productService.create(Product.builder().name("americano").productcategory(ProductCategory.DRINKS).build());
+        Product orangeJuice = productService.create(Product.builder().name("orangeJuice").productcategory(ProductCategory.DRINKS).build());
+        Product tzatziki = productService.create(Product.builder().name("tzatziki").productcategory(ProductCategory.SAUCE).build());
 
         StoreCategory tavern = storeCategoryService.create(StoreCategory.builder().description("Tavern").build());
         StoreCategory coffeeShop = storeCategoryService.create(StoreCategory.builder().description("Coffee Shop").build());
@@ -53,92 +66,77 @@ public class SampleContent extends BaseComponent implements CommandLineRunner {
         StoreCategory souvlatzidiko = storeCategoryService.create(StoreCategory.builder().description("Souvlatzidiko").build());
         storeCategoryService.findAll().forEach(sc -> log.info("Store category {} created.", sc));
 
-        Store louloudaki = storeService.create(Store.builder().name("Louloudaki").storeCategory(tavern).address("Plateia Eleftheris 51 71303 Athens Attica").build());
-        Store xelona = storeService.create(Store.builder().name("Xelona").storeCategory(tavern).address("Analipsi 7 70303 Athens").build());
-        Store kagiampis = storeService.create(Store.builder().name("Kagiampis").storeCategory(tavern).address("Plateia Kornarou 51 71203 Athens Attica").build());
-        Store pratos = storeService.create(Store.builder().name("Pratos").storeCategory(iceCreamShop).address("Dikaiosinis 51 70201 Athens Attica").build());
-        storeCategoryService.findAll().forEach(s -> log.info("Stores {} created.", s));
+//        Store louloudaki = storeService.create(Store.builder().name("Louloudaki").storeCategory(tavern).address("Plateia Eleftheris 51 71303 Athens Attica").build());
+//        Store xelona = storeService.create(Store.builder().name("Xelona").storeCategory(tavern).address("Analipsi 7 70303 Athens").build());
+//        Store kagiampis = storeService.create(Store.builder().name("Kagiampis").storeCategory(tavern).address("Plateia Kornarou 51 71203 Athens Attica").build());
+//        Store pratos = storeService.create(Store.builder().name("Pratos").storeCategory(iceCreamShop).address("Dikaiosinis 51 70201 Athens Attica").build());
 
-        ProductCategory salad = productCategoryService.create(ProductCategory.builder().description("Salad").build());
-        ProductCategory sideDish = productCategoryService.create(ProductCategory.builder().description("Side Dish").build());
-        ProductCategory souvlaki = productCategoryService.create(ProductCategory.builder().description("Souvlaki").build());
-        ProductCategory iceCream = productCategoryService.create(ProductCategory.builder().description("Ice Cream").build());
-        ProductCategory sauce = productCategoryService.create(ProductCategory.builder().description("Sauce").build());
-        ProductCategory oven = productCategoryService.create(ProductCategory.builder().description("Oven dish").build());
-        ProductCategory roastMeat = productCategoryService.create(ProductCategory.builder().description("Roast meat").build());
-        ProductCategory drinks = productCategoryService.create(ProductCategory.builder().description("Drinks").build());
-        storeCategoryService.findAll().forEach(pc -> log.info("Product category {} created.", pc));
+        Store louloudaki = storeService.initiateStore("Louloudaki",tavern,"Plateia Eleftheris 51 71303 Athens Attica");
+        louloudaki.setImage("path to flower image");
+        storeService.addStoreProduct(louloudaki,xoriatiki,"Louloudaki Salad","With vinegar sauce and manouri cheese", BigDecimal.valueOf(4.5),"");
+        storeService.addStoreProduct(louloudaki,liver,"Fried Liver","empty", BigDecimal.valueOf(5),"");
+        storeService.addStoreProduct(louloudaki,zucchini,"Fried Zucchini","empty", BigDecimal.valueOf(3),"");
+        storeService.addStoreProduct(louloudaki,redWine,"Red wine","250 ml", BigDecimal.valueOf(2),"");
+        storeService.create(louloudaki);
 
-        List<Product> products = List.of(Product.builder().serial("30-0-00000-1-0001").name("Louloudaki Salad").price(BigDecimal.valueOf(4))
-                                                .productcategory(salad).store(louloudaki).build(),
-                                        Product.builder().serial("30-0-00000-1-0002").name("Liver").price(BigDecimal.valueOf(6))
-                                                .productcategory(sideDish).store(louloudaki).build(),
-                                        Product.builder().serial("30-0-00000-1-0003").name("Fried Zucchini").price(BigDecimal.valueOf(3.5))
-                                                .productcategory(sideDish).store(louloudaki).build(),
-                                        Product.builder().serial("30-0-00000-1-0004").name("Red wine").price(BigDecimal.valueOf(4))
-                                                .productcategory(drinks).store(louloudaki).build(),
-                                        Product.builder().serial("30-0-00000-1-0005").name("Souvlaki kotopoulo").price(BigDecimal.valueOf(4))
-                                                .productcategory(souvlaki).store(louloudaki).build(),
-                                        Product.builder().serial("30-0-00000-2-0001").name("vanillia").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(iceCream).store(pratos).build(),
-                                        Product.builder().serial("30-0-00000-2-0002").name("chocolate").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(iceCream).store(pratos).build(),
-                                        Product.builder().serial("30-0-00000-2-0003").name("mango").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(iceCream).store(pratos).build(),
-                                        Product.builder().serial("30-0-00000-3-0001").name("Tzatziki").price(BigDecimal.valueOf(4.5))
-                                                .productcategory(sauce).store(xelona).build(),
-                                        Product.builder().serial("30-0-00000-3-0002").name("Greek cofee").price(BigDecimal.valueOf(2.5))
-                                                 .productcategory(drinks).store(xelona).build(),
-                                        Product.builder().serial("30-0-00000-3-0003").name("Fried potatos").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(sideDish).store(xelona).build(),
-                                        Product.builder().serial("30-0-00000-3-0004").name("Red Wine").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(drinks).store(xelona).build(),
-                                        Product.builder().serial("30-0-00000-3-0005").name("Papoutsakia").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(oven).store(xelona).build(),
-                                        Product.builder().serial("30-0-00000-3-0006").name("Paidakia").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(roastMeat).store(xelona).build(),
-                                        Product.builder().serial("30-0-00000-4-0001").name("Greek Coffee").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(drinks).store(kagiampis).build(),
-                                        Product.builder().serial("30-0-00000-4-0002").name("Americano").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(drinks).store(kagiampis).build(),
-                                        Product.builder().serial("30-0-00000-4-0003").name("Orange juice").price(BigDecimal.valueOf(2.5))
-                                                .productcategory(drinks).store(kagiampis).build());
-        log.info("Accounts created: {}.", productService.createAll(products));
-        System.out.println(productService.findBySerial("30-0-00000-4-0002"));
+        PaymentCard.builder().build();
+
+//                StoreProduct.builder().name("Souvlaki kotopoulo").price(BigDecimal.valueOf(4))
+//                        .product(souvlakiKotopoulo).store(louloudaki).build(),
+//                StoreProduct.builder().name("Pratos vanillia ice cream").price(BigDecimal.valueOf(2.5))
+//                        .product(vanilliaFlavor).store(pratos).build(),
+//                StoreProduct.builder().name("Pratos chocolate ice cream").price(BigDecimal.valueOf(2.5))
+//                        .product(chocolateFlavor).store(pratos).build(),
+//                StoreProduct.builder().name("Tzatziki").price(BigDecimal.valueOf(4.5))
+//                        .product(tzatziki).store(xelona).build(),
+//                StoreProduct.builder().name("Greek coffee").price(BigDecimal.valueOf(2.5))
+//                        .product(greekCoffee).store(xelona).build(),
+//                StoreProduct.builder().name("Red Wine").price(BigDecimal.valueOf(2.5))
+//                        .product(redWine).store(xelona).build(),
+//                StoreProduct.builder().name("Papoutsakia").price(BigDecimal.valueOf(2.5))
+//                        .product(papoutsakia).store(xelona).build(),
+//                StoreProduct.builder().name("Paidakia").price(BigDecimal.valueOf(2.5))
+//                        .product(paidakia).store(xelona).build(),
+//                StoreProduct.builder().name("Greek Coffee").price(BigDecimal.valueOf(2.5))
+//                        .product(greekCoffee).store(kagiampis).build(),
+//                StoreProduct.builder().name("Americano").price(BigDecimal.valueOf(2.5))
+//                        .product(americano).store(kagiampis).build(),
+//                StoreProduct.builder().name("Orange juice").price(BigDecimal.valueOf(2.5))
+//                        .product(orangeJuice).store(kagiampis).build());
 
         Account firstAccount = accountService.findByEmail("mommykmr@hotmail.red");
         Order firstOrder = orderService.initiateOrder(firstAccount);
         firstOrder.setSerial("0000010001");
 
-        orderService.addItem(firstOrder, productService.findBySerial("30-0-00000-1-0001"), 2);
-        orderService.addItem(firstOrder, productService.findBySerial("30-0-00000-1-0002"), 1);
-        orderService.addItem(firstOrder, productService.findBySerial("30-0-00000-1-0003"), 1);
+        orderService.addItem(firstOrder, storeService.getStoreProduct(louloudaki,4L), 2);
+
 //        // Update item(s)
 //        orderService.addItem(firstOrder, productService.findBySerial("30-0-00000-1-0002"), 1);
 //        orderService.updateItem(firstOrder, productService.findBySerial("30-0-00000-1-0002"), 2);
 //        // Remove item(s)
 //        orderService.removeItem(firstOrder, productService.findBySerial("30-0-00000-1-0003"));
+
         orderService.proceedToCheckout(firstOrder,PaymentMethod.CASH);
         orderService.checkout(firstOrder);
-
-        Account secondCustomer = accountService.get(2L);
-        Order secondOrder = orderService.initiateOrder(secondCustomer);
-        secondOrder.setSerial("0000010002");
-
-        orderService.addItem(secondOrder, productService.findBySerial("30-0-00000-2-0001"), 1);
-        orderService.addItem(secondOrder, productService.findBySerial("30-0-00000-2-0002"), 1);
-        orderService.proceedToCheckout(secondOrder,PaymentMethod.CASH);
-        orderService.checkout(secondOrder);
-
-        Account thirdAccount = accountService.findByPhoneNumber("6984072273");
-        Order thirdOrder = orderService.initiateOrder(thirdAccount);
-        thirdOrder.setSerial("0000010003");
-
-        orderService.addItem(thirdOrder, productService.findBySerial("30-0-00000-3-0001"), 3);
-        orderService.addItem(thirdOrder, productService.findBySerial("30-0-00000-3-0002"), 2);
-        orderService.addItem(thirdOrder, productService.findBySerial("30-0-00000-3-0003"), 1);
-        orderService.proceedToCheckout(thirdOrder,PaymentMethod.PAYMENT_CARD);
-        orderService.checkout(thirdOrder);
+//
+//        Account secondCustomer = accountService.get(2L);
+//        Order secondOrder = orderService.initiateOrder(secondCustomer);
+//        secondOrder.setSerial("0000010002");
+//
+//        orderService.addItem(secondOrder, productService.findBySerial("30-0-00000-2-0001"), 1);
+//        orderService.addItem(secondOrder, productService.findBySerial("30-0-00000-2-0002"), 1);
+//        orderService.proceedToCheckout(secondOrder,PaymentMethod.CASH);
+//        orderService.checkout(secondOrder);
+//
+//        Account thirdAccount = accountService.findByPhoneNumber("6984072273");
+//        Order thirdOrder = orderService.initiateOrder(thirdAccount);
+//        thirdOrder.setSerial("0000010003");
+//
+//        orderService.addItem(thirdOrder, productService.findBySerial("30-0-00000-3-0001"), 3);
+//        orderService.addItem(thirdOrder, productService.findBySerial("30-0-00000-3-0002"), 2);
+//        orderService.addItem(thirdOrder, productService.findBySerial("30-0-00000-3-0003"), 1);
+//        orderService.proceedToCheckout(thirdOrder,PaymentMethod.PAYMENT_CARD);
+//        orderService.checkout(thirdOrder);
 
     }
 }
