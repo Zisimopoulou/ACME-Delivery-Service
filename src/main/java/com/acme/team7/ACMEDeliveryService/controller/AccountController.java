def5f384663170a353845f7d1f3a06146e548b83
Petros Dhespollari <peterdsp@icloud.com>
2022-09-05 13:14:48 +0300

    Update AccountController

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/AccountController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/AccountController.java
@@ -1,36 +1,12 @@
 package com.acme.team7.ACMEDeliveryService.controller;
 
 import com.acme.team7.ACMEDeliveryService.domain.Account;
-import com.acme.team7.ACMEDeliveryService.service.AccountService;
+import com.acme.team7.ACMEDeliveryService.domain.Order;
 import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.NoSuchElementException;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("ACCOUNTS")
-public class AccountController {
-    private final AccountService accountService;
-    @GetMapping(params = "email")
-    public ResponseEntity<ApiResponse<Account>> findByEmail(@RequestParam String email) {
-        final Account byEmail = accountService.findByEmail(email);
-        if (byEmail== null) {
-            throw new NoSuchElementException("Email not found");
-        }
-        return ResponseEntity.ok(ApiResponse.<Account>builder().data(byEmail).build());
-    }
-    @GetMapping(params = "phoneNumber")
-    public ResponseEntity<ApiResponse<Account>> findPhoneNumber(@RequestParam String phoneNumber) {
-        final Account byPhoneNumber = accountService.findByEmail(phoneNumber);
-        if (byPhoneNumber== null) {
-            throw new NoSuchElementException("Phone not found");
-        }
-        return ResponseEntity.ok(ApiResponse.<Account>builder().data(byPhoneNumber).build());
-    }
+public interface AccountController extends BaseController<Account> {
+    ResponseEntity<ApiResponse<Account>> findByEmail(@RequestParam String email);
+    ResponseEntity<ApiResponse<Account>> findPhoneNumber(@RequestParam String phoneNumber);
 }
