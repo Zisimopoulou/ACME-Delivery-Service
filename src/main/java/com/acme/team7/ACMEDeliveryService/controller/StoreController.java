9a1efebcd6d050252b0d73091985f7ccd8bcc99b
Petros Dhespollari <peterdsp@icloud.com>
2022-09-06 14:13:02 +0300

    Update StoreController

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreController.java
@@ -1,14 +1,17 @@
 package com.acme.team7.ACMEDeliveryService.controller;
 
-import com.acme.team7.ACMEDeliveryService.domain.Store;
+import com.acme.team7.ACMEDeliveryService.domain.*;
 import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface StoreController {
-//    ResponseEntity<ApiResponse<List<Store>>> findByName(@RequestParam String name);
-//
-//    ResponseEntity<ApiResponse<List<Store>>> findStoreByStoreCategory_Description(@RequestParam String description);
+  ResponseEntity<ApiResponse<List<Store>>> findStoreByStoreCategory_Description(String name, String description);
+  ResponseEntity<ApiResponse<Store>> initiateStore(String name, StoreCategory storeCategory, String address);
+  void addStoreProduct(Store store, Product product, String name, String details, BigDecimal price, String image);
+  void updateStoreProductPrice(Store store, StoreProduct storeProduct, BigDecimal price);
+  ResponseEntity<ApiResponse<StoreProduct>> getStoreProduct(Store store, Long id);
+  void removeStoreProduct(Store store, StoreProduct storeProduct);
 }
