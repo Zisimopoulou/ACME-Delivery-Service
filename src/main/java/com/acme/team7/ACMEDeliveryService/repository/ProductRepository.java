b4d09e8cdc8aa213d7bb25ebcbf4a4ce1746fcb3
Petros Dhespollari <info@peterdsp.dev>
2022-09-07 16:20:37 +0300

    Update ProductRepository

--- src/main/java/com/acme/team7/ACMEDeliveryService/repository/ProductRepository.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/repository/ProductRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//    @Query(nativeQuery = true)
-//
-//    List<Product> reportTopProducts();
+//  @Query(value= "SELECT
+//  STOREPRODUCT_ID,COUNT(STOREPRODUCT_ID) Frequency"+"FROM ORDER_ITEMS"+"GROUP BY STOREPRODUCT_ID"+"ORDER BY COUNT(STOREPRODUCT_ID) DESC"+"FETCH NEXT 1 ROWS ONLY", nativeQuery = true)
+    @Query(nativeQuery = false)
+    List<Product> reportTopProducts();
     Product findProductByName(String name);
 }
 
