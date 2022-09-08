884d7d61ffe0ec6310708397082d106d3da1d29b
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-08 13:16:13 +0300

    Create top store products class to display the respective report outcome.

---
+++ src/main/java/com/acme/team7/ACMEDeliveryService/transfer/TopStoreProducts.java
@@ -0,0 +1,8 @@
+package com.acme.team7.ACMEDeliveryService.transfer;
+
+import java.math.BigDecimal;
+
+public interface TopStoreProducts {
+    String getStoreProductId();
+    BigDecimal getFrequency();
+}
