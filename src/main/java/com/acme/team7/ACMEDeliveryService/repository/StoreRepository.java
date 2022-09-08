ad0f599d1487ab3e768f33eed002f9d695a46c21
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-08 16:10:18 +0300

    Add query, to report most famous stores depending on the orders. Add variable nameOrId to extract info about the name of store product or the id of the store.

--- src/main/java/com/acme/team7/ACMEDeliveryService/repository/StoreRepository.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/repository/StoreRepository.java
@@ -16,17 +16,29 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     Store findStoreById(Long id);
 
     @Query(
-            value = "  SELECT STOREPRODUCT_ID as storeProductId,COUNT(STOREPRODUCT_ID) Frequency \\\n" +
-                    "  FROM ORDER_ITEMS \\\n" +
-                    "  GROUP BY STOREPRODUCT_ID \\\n" +
-                    "  ORDER BY COUNT(STOREPRODUCT_ID) DESC \\\n" +
+            value = "  SELECT STOREPRODUCT_ID, STOREPRODUCT.NAME as nameOrId,COUNT(STOREPRODUCT_ID) frequency \n" +
+                    "  FROM ORDER_ITEMS \n" +
+                    "  INNER JOIN STOREPRODUCT ON STOREPRODUCT.ID=ORDER_ITEMS.STOREPRODUCT_ID \n" +
+                    "  GROUP BY STOREPRODUCT_ID, STOREPRODUCT.NAME \n" +
+                    "  ORDER BY COUNT(STOREPRODUCT_ID) DESC \n" +
                     "  FETCH NEXT 10 ROWS ONLY",
             nativeQuery = true)
     List<TopStoreProducts> reportTop10StoreProducts();
 
-//    @Query(nativeQuery = true)
-//    List<Store> reportTopStores();
-//
+    @Query(
+            value = "  SELECT store_id as nameOrId, COUNT(store_id) as frequency \n" +
+                    "  FROM (\n" +
+                    "  SELECT order_id, store_id \n" +
+                    "  FROM ORDER_ITEMS \n" +
+                    "  INNER JOIN STOREPRODUCT ON storeproduct.id=order_items.storeproduct_id \n" +
+                    "  Group by order_id, store_id \n" +
+                    "  ) \n" +
+                    "  GROUP BY store_id \n" +
+                    "  ORDER BY frequency DESC \n" +
+                    "  FETCH NEXT 20 ROWS ONLY",
+            nativeQuery = true)
+    List<TopStoreProducts> reportTopStores();
+
 //    @Query(nativeQuery = true)
 //    List<Store> reportTopStoresPerCategory();
 }
