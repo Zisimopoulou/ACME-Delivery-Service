0693885aa1493033de880e82511f4c3c78eab9b2
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-09-09 10:54:33 +0300

    Add report top store products and stores methods to store controller.

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreController.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreController.java
@@ -2,6 +2,7 @@ package com.acme.team7.ACMEDeliveryService.controller;
 
 import com.acme.team7.ACMEDeliveryService.domain.*;
 import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import com.acme.team7.ACMEDeliveryService.transfer.TopReports;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface StoreController {
   ResponseEntity<ApiResponse<List<Store>>> findStoreByStoreCategory_Description(String name, String description);
   ResponseEntity<ApiResponse<StoreProduct>> getStoreProduct(Store store, Long id);
+  ResponseEntity<ApiResponse<List<TopReports>>> reportTop10StoreProducts();
+  ResponseEntity<ApiResponse<List<TopReports>>> reportTopStores();
 }
--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreControllerImpl.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/StoreControllerImpl.java
@@ -4,6 +4,7 @@ import com.acme.team7.ACMEDeliveryService.domain.*;
 import com.acme.team7.ACMEDeliveryService.service.BaseService;
 import com.acme.team7.ACMEDeliveryService.service.StoreService;
 import com.acme.team7.ACMEDeliveryService.transfer.ApiResponse;
+import com.acme.team7.ACMEDeliveryService.transfer.TopReports;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("stores")
+@RequestMapping("STORES")
 public class StoreControllerImpl extends BaseControllerImpl<Store> implements StoreController {
 
     private final StoreService storeService;
@@ -35,9 +37,21 @@ public class StoreControllerImpl extends BaseControllerImpl<Store> implements St
     }
 
     @Override
-    public ResponseEntity<ApiResponse<StoreProduct>>  getStoreProduct(Store store, Long id){
+    public ResponseEntity<ApiResponse<StoreProduct>> getStoreProduct(Store store, Long id){
         return new ResponseEntity<>(ApiResponse.<StoreProduct>builder().
                 data(storeService.getStoreProduct(store, id)).build(), HttpStatus.CREATED);
+    }
+
+    @Override
+    @GetMapping("reportTop10StoreProducts")
+    public ResponseEntity<ApiResponse<List<TopReports>>> reportTop10StoreProducts() {
+        return null;
+    }
+
+    @Override
+    @GetMapping("reportTopStores")
+    public ResponseEntity<ApiResponse<List<TopReports>>> reportTopStores() {
+        return null;
     }
 
 }
