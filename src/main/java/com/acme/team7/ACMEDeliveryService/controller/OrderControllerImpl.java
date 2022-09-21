3ae3773e274235e130cf5437c7aedd1f282e4a32
Petros Dhespollari <info@peterdsp.dev>
2022-09-21 22:47:05 +0300

    Update OrdersControllersImpl

--- src/main/java/com/acme/team7/ACMEDeliveryService/controller/OrderControllerImpl.java
+++ src/main/java/com/acme/team7/ACMEDeliveryService/controller/OrderControllerImpl.java
@@ -1,5 +1,6 @@
 package com.acme.team7.ACMEDeliveryService.controller;
 
+import com.acme.team7.ACMEDeliveryService.domain.Account;
 import com.acme.team7.ACMEDeliveryService.domain.Order;
 import com.acme.team7.ACMEDeliveryService.service.BaseService;
 import com.acme.team7.ACMEDeliveryService.service.OrderService;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,6 +32,14 @@ public class OrderControllerImpl extends BaseControllerImpl<Order> {
     public ResponseEntity<ApiResponse<Order>> create(@Valid @RequestBody final Order entity) {
         return new ResponseEntity<>(ApiResponse.<Order>builder().data(orderService.checkout(entity)).build(),
                 HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<ApiResponse<List<Order>>> findOrdersByAccount(@RequestParam Account account) {
+        final List<Order> orders = orderService.findOrdersByAccount(account);
+        if (orders == null) {
+            throw new NoSuchElementException("No orders found");
+        }
+        return ResponseEntity.ok(ApiResponse.<List<Order>>builder().data(orders).build());
     }
 }
 
